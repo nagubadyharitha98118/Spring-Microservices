080de07de72160e3d75ac53fb9c5df67facebdb8
nagubadyharitha98118 <nagubadyharitha98118@gmail.com>
2021-11-24 10:46:09 +0530

    Initial code base

---
+++ department-service/src/main/java/com/haritha/department/repository/DepartmentRepository.java
@@ -0,0 +1,10 @@
+package com.haritha.department.repository;
+
+import com.haritha.department.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    Department findByDepartmentId(Long departmentId);
+}
---
+++ department-service/src/main/java/com/haritha/department/service/DepartmentService.java
@@ -0,0 +1,28 @@
+package com.haritha.department.service;
+
+import com.haritha.department.entity.Department;
+import com.haritha.department.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public Department saveDepartment(Department department) {
+        log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.save(department);
+    }
+
+    public Department findDepartmentById(Long departmentId) {
+        log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.findByDepartmentId(departmentId);
+    }
+}
